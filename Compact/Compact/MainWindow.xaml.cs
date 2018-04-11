0a68b94bd7e972d7bf9c3b4d503a0866936bc573
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-04-11 19:43:44 +0200

    Initial commit

---
+++ Compact/Compact/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Effects;
+
+namespace Compact
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Canvas CreateBundleEntry(BundleEntry entry)
+        {
+            Canvas mainCanvas = new Canvas()
+            {
+                Width = 350,
+                Height = 90,
+                Background = new ImageBrush((ImageSource) new ImageSourceConverter()
+                    .ConvertFromString(entry.BackgroundImage.AbsoluteUri)),
+                Effect = new DropShadowEffect()
+                {
+                    BlurRadius = 20,
+                    RenderingBias = RenderingBias.Performance,
+                    Opacity = 0.25
+                }
+            };
+
+            TextBlock nameLabel = new TextBlock()
+            {                
+                Text = entry.Name,
+                TextWrapping = TextWrapping.NoWrap,
+                TextTrimming = TextTrimming.CharacterEllipsis,
+                Width = 320,
+                Height = 90,
+                Foreground = entry.ForegroundColor,
+                FontFamily = new FontFamily("Segoe UI Semilight"),
+                FontSize = 24.0
+            };
+
+            mainCanvas.Margin = new Thickness(15);
+            mainCanvas.Children.Add(nameLabel);
+            return mainCanvas;
+        }
+
+        private void OnWindowLoad(object sender, RoutedEventArgs e)
+        {
+            // TO-DO: Load packages from JSON file
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Compact/Compact/Models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Compact
+{
+    public class BundleEntry
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Id { get; set; }
+        public string Icon { get; set; }
+        public Uri BackgroundImage { get; set; }
+        public SolidColorBrush ForegroundColor { get; set; }
+    }
+}

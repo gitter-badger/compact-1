33edde72f5fbde4b76372e63e77abaf41f71a345
Kees van Voorthuizen <keesvv@users.noreply.github.com>
2018-04-13 23:58:31 +0200

    Added serialization class

---
+++ Compact/Serialization.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Compact
+{
+    [Serializable()]
+    public class Bundle
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("banner_url")]
+        public string BannerUrl { get; set; }
+
+        [JsonProperty("software")]
+        public List<string> SoftwareList { get; set; }
+
+        [JsonProperty("installers")]
+        public List<string> InstallerList { get; set; }
+    }
+}

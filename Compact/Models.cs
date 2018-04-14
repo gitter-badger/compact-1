7b01455e234342c526037e8243a6ad0d4642f8b2
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-04-14 20:13:07 +0200

    Re-added models class

---
+++ Compact/Models.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compact
+{
+    public class BundleProgress
+    {
+        public int ProgressPercentage { get; set; }
+        public SoftwareListItem CurrentItem { get; set; }
+    }
+}

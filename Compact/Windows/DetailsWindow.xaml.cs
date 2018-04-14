5912ed3fd5be53100ec5bb1fd8fcd6860fdef2ac
Kees van Voorthuizen <keesvv@users.noreply.github.com>
2018-04-14 14:27:48 +0200

    Added functionality

--- Compact/Windows/DetailsWindow.xaml.cs
+++ Compact/Windows/DetailsWindow.xaml.cs
@@ -1,27 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.IO;
 
 namespace Compact.Windows
 {
-    /// <summary>
-    /// Interaction logic for DetailsWindow.xaml
-    /// </summary>
     public partial class DetailsWindow : Window
     {
         public DetailsWindow()
         {
             InitializeComponent();
+        }
+
+        public SoftwareListItem[] SoftwareList { get; set; }
+        public List<SoftwareListItem> SoftwareListQueue { get; set; } = new List<SoftwareListItem>();
+
+        private void OnClickInstall(object sender, RoutedEventArgs e)
+        {
+            // Iterates through all selected values
+            foreach (Canvas canvas in lstSoftware.Items)
+            {
+                try
+                {
+                    foreach (CheckBox checkBox in canvas.Children)
+                    {
+                        if (checkBox.IsChecked.Value == true)
+                        {
+                            SoftwareListItem queueItem = (SoftwareListItem)checkBox.Tag;
+                            SoftwareListQueue.Add(queueItem);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                }          
+            }
+
+            // Disable all user input
+            IsEnabled = false;
+
+            // Download all installers
+            foreach (SoftwareListItem item in SoftwareListQueue)
+            {
+                string dataFolder = Utilities.GetDataFolder();
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile(new Uri(item.Url), dataFolder);
+            }
+
+            // Clear the software queue and enable user input
+            SoftwareListQueue.Clear();
+            IsEnabled = true;
         }
     }
 }

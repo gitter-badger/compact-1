131d56ad0cb8079046513424b8e63869d1025bd1
Kees van Voorthuizen <keesvv@users.noreply.github.com>
2018-04-14 14:26:45 +0200

    Added Utilities class

---
+++ Compact/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compact
+{
+    public class Utilities
+    {
+        public static string GetDataFolder()
+        {
+            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Compact");
+
+            try
+            {
+                if (!Directory.Exists(folderPath))
+                    Directory.CreateDirectory(folderPath);
+            }
+            catch (Exception)
+            {
+            }
+
+            return folderPath;
+        }
+    }
+}
